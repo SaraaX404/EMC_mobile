8c6ebe2d9128a5ebe5e4dce2bc775519966eb761
SaraaX404 <thathsarasenanayaka74@gmail.com>
2023-05-28 11:25:55 +0530

    fetch data

--- index.php
+++ index.php
@@ -1,20 +1,31 @@
 <!DOCTYPE html>
 <html lang="en">
 <?php include'head.php'?>
+
 <body>
     <div style="padding:150px; padding-top:50px;">
     <select class="form-control mt-5 mb-5" id="exampleFormControlSelect1">
+        
       <option>Matara</option>
       <option>Galle</option>
       <option>Hambanthota</option>
     </select>
-    <div class="card" style="width: 18rem;">
-  <img class="card-img-top" src="https://th.bing.com/th/id/OIP.d70cdHffTDV33-VyYfAjyAHaJo?pid=ImgDet&rs=1" alt="Card image cap" style="width:150px">
-  <div class="card-body">
-    <h5 class="card-title">Brand - Name</h5>
-    <a href="#" class="btn btn-primary">Order Now</a>
-  </div>
-    </div>
+    <?php
+    $sql = "SELECT * FROM mobile";
+    $result = mysqli_query($conn, $sql);
+    while ($row = mysqli_fetch_assoc($result)) {
+        echo '<div class="card" style="width: 18rem;">
+        <img class="card-img-top" src="https://th.bing.com/th/id/OIP.d70cdHffTDV33-VyYfAjyAHaJo?pid=ImgDet&rs=1" alt="Card image cap" style="width:150px">
+        <div class="card-body">
+          <h5 class="card-title">.'$row['brand']'. - .'$row['name']'.</h5>
+          <a href="#" class="btn btn-primary">Order Now</a>
+        </div>
+          </div>'
+    }
+    ?>
+
+
+  
     </div>
 </body>
 </html>
